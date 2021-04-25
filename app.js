87e976ba0ae5bc7c44274725a7086b46e9763a00
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-04-25 14:51:19 +0300

    dem

---
+++ app.js
@@ -0,0 +1,19 @@
+alert('welcam to our web');
+
+let userName = prompt('your name please?');
+console.log(userName);
+
+let age = prompt('what is your age?');
+console.log(age);
+
+let home = prompt('current home?');
+console.log(home);
+
+let status = prompt('Marital status?');
+console.log(status);
+
+let education = prompt('your education please?');
+console.log(education);
+
+
+alert('Hello ' + userName + ' your age ' + age + ' and current home is ' + home + ' Marital status ' + status + ' your education ' + education );
